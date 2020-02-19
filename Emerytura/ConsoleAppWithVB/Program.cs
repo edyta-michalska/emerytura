7a3494517585a672b0b4eff1c4c061be020e38c5
edyta-michalska <e.michalska@microsoft.wsei.edu.pl>
2020-02-19 18:45:42 +0100

    cos nowego

---
+++ Emerytura/ConsoleAppWithVB/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualBasic;
+
+namespace ConsoleAppWithVB
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //tu piszemy kod
+            //Console.OutputEncoding = Encoding.UTF8
+
+            Interaction.MsgBox("Program emerytura");
+
+            // Console.WriteLine("program EMERYTURA");
+            //Console.Write("podaj swoje imie: ");
+            string imie;
+            //imie = Console.ReadLine().Trim().ToUpper();  //kropka działa jako separator kolejnych kroków
+            //imie = imie.Trim();  //przycina spacje przypisuje imie przycięte o spacje imie i zapamietuje teraz przyciete imie
+            imie = Interaction.InputBox("podaj imie", "tytuł okienka").Trim().ToUpper();
+
+
+
+            //Console.Write("podaj swoje nazwisko: ");
+            string nazwisko = Interaction.InputBox("podaj nazwisko", "tytuł okienka").Trim();
+            string PierwszaLitera = nazwisko.Substring(0, 1);
+            string reszta = nazwisko.Substring(1);
+            nazwisko = PierwszaLitera.ToUpper() + reszta.ToLower();
+
+            //Console.WriteLine("Witaj, " + imie + " " + nazwisko + "!");
+
+            //Console.WriteLine("Witaj, {0} {1}", nazwisko, imie); //formatted string
+            Console.WriteLine($"Witaj, {imie} {nazwisko}");
+
+            //powyższe to sposoby na sklejanie tekstu. obacnie używamy ostatniej opcji, nie plusami bo spowalniają program
+            //Console.Write("Podaj swój wiek: ");
+            int wiek; 
+            string s = Interaction.InputBox("podaj wiek", "tytuł okienka");
+            wiek = int.Parse(s); //funkcja int.parse konwertuje na liczbę ale musimy chceć przekonwertować tekst 50 na liczbę 50
+
+            Console.WriteLine($"Masz { wiek} lat");
+
+            //jezeli wiek > 67 to jestes emerytem
+            //a w przeciwnym przypadku do emerytury zostalo ci 67-wiek lat
+            if (wiek >= 67)
+            {
+                Console.WriteLine("jestes emerytem");
+            }
+            else
+            {
+                Console.WriteLine($"do emerytury zostało ci {67 - wiek} lat");
+
+
+            }
+        }
+    }
+
+}
--- Emerytura/ConsoleAppWithVB/Properties/AssemblyInfo.cs
+++ Emerytura/ConsoleAppWithVB/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("EmeryturaSolution")]
+[assembly: AssemblyTitle("ConsoleAppWithVB")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("EmeryturaSolution")]
+[assembly: AssemblyProduct("ConsoleAppWithVB")]
 [assembly: AssemblyCopyright("Copyright ©  2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("91d559d8-8778-46f2-80e6-377309a38cb1")]
+[assembly: Guid("b255327c-4415-438d-a2b1-4623387525c8")]
 
 // Version information for an assembly consists of the following four values:
 //
